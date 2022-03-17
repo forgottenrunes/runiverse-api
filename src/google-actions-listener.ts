b2f0aff555306dfb49b37688129b8891f9e3c7bc
wagmiwiz <wagmiwiz@users.noreply.github.com>
2022-03-17 15:13:43 +0000

    Refactor of voice fulfillment functions to work with voiceflow.com.

--- src/google-actions-listener.ts
+++ src/google-actions-listener.ts
@@ -4,8 +4,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import { WIZARDS_CONTRACT } from "./metadata/wizards";
 
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import markdownToTxt from "./lib/markdown";
+import { SOULS_CONTRACT } from "./metadata/souls";
+import { PONIES_CONTRACT } from "./metadata/ponies";
 
 dotenv.config();
 
@@ -70,7 +72,105 @@ app.handle("lore_handler", async (conv) => {
 });
 
 const expressApp = express().use(bodyParser.json());
-expressApp.post("/fulfillment", app);
+expressApp.post("/fulfillment", app); // for google actions
+
+async function getTokenFromDb(tokenType: string, wizardId: number) {
+  return await prisma.token.findUnique({
+    where: {
+      tokenContract_tokenId: {
+        tokenContract:
+          tokenType === "soul"
+            ? SOULS_CONTRACT.toLowerCase()
+            : tokenType === "pony"
+            ? PONIES_CONTRACT.toLowerCase()
+            : WIZARDS_CONTRACT.toLowerCase(),
+        tokenId: wizardId,
+      },
+    },
+    select: { id: true },
+  });
+}
+
+async function getTokenNameFromDb(tokenType: string, internalTokenId: string) {
+  const queryParams = {
+    where: {
+      tokenId: internalTokenId,
+    },
+    select: { name: true },
+  };
+
+  const response = await (tokenType === "soul"
+    ? prisma.soul.findUnique(queryParams)
+    : tokenType === "pony"
+    ? prisma.pony.findUnique(queryParams)
+    : prisma.wizard.findUnique(queryParams));
+
+  return response?.name ?? "Unknown Character";
+}
+
+async function getLoreForToken(internalTokenId: string) {
+  const lore = await prisma.lore.findMany({
+    where: {
+      tokenId: internalTokenId,
+    },
+    orderBy: { index: "asc" },
+  });
+  return lore;
+}
+
+const TOKEN_NOT_FOUND = "token_not_found";
+const NO_LORE = "no_lore";
+
+expressApp.post("/lore-fulfillment", async (req, res) => {
+  console.log(`Lore fulfilment request:`);
+  console.log(req.body);
+
+  if (!req.body.tokenId || isNaN(parseInt(req.body.tokenId))) {
+    return res.json({
+      status: TOKEN_NOT_FOUND,
+    });
+  }
+
+  const tokenId = parseInt(req.body.tokenId);
+  const tokenType = req.body.tokenType.toLowerCase();
+
+  const token = await getTokenFromDb(tokenType, tokenId);
+
+  if (!token) {
+    return res.json({
+      status: TOKEN_NOT_FOUND,
+    });
+  }
+
+  const lore = await getLoreForToken(token.id);
+  const name = await getTokenNameFromDb(tokenType, token.id);
+
+  if (lore.length === 0) {
+    return res.json({
+      status: NO_LORE,
+      name: name,
+    });
+  }
+
+  return res.json({
+    lore: `${markdownToTxt(lore[0].markdownText ?? "Empty lore...")}`,
+    name: name,
+  });
+});
+
+expressApp.post("/recent-lore-fulfillment", async (req, res) => {
+  console.log(`Recent lore fulfilment request`);
+
+  const lores: any[] = await prisma.$queryRaw(
+    Prisma.sql`select "markdownText" from "PaginatedLore" as l where  "nsfw" = false and length("markdownText") > 50 order by "createdAtBlock" desc limit 1;`
+  );
+
+  return res.json({
+    lore: `${markdownToTxt(lores[0].markdownText ?? "Empty lore...")}`,
+  });
+});
+
+// delete once new endpoint above is out
 expressApp.post("/alexa-fulfillment", async (req, res) => {
   console.log(req.body);
   if (!req.body.wizardId || isNaN(parseInt(req.body.wizardId))) {
@@ -93,13 +193,6 @@ expressApp.post("/alexa-fulfillment", async (req, res) => {
       lore: "Whoops, no lore found for this wizard id... goodbye",
     });
   }
-
-  const wizard = await prisma.wizard.findUnique({
-    where: {
-      tokenId: token.id,
-    },
-    select: { name: true },
-  });
 
   const lore = await prisma.lore.findMany({
     where: {
